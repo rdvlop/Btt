8599f85a29d553fa086980336f872ecce8752633
Rida Moumen <ridamoumen@MacBook-Pro-de-Rida.local>
2019-04-11 16:08:50 +0200

    regler

--- Inscription/creation-user.php
+++ Inscription/creation-user.php
@@ -1,23 +1,7 @@
 <?php
 
 
-//require "../DBConnexion.php" ;
-
-$host = "localhost"; 
-$user = "root";
-$mdp = "root";
-$bdd = "Batuta";
-$mysqli;
-
-function connection() {
-    echo "<p>connexion...</p>"
-    $mysqli = new mysqli($host, $user, $mdp, $bdd ) ;
-    echo "<p>connecté...</p>"
-    if ( $mysqli->connect_errno ) {
-        die ("<p> Impossible de connecter à $bdd : " . $mysqli->connect_error . " </p>" ) ; 
-    }
-    return $mysqli;
-}
+require "../DBConnexion.php" ;
 
 $mysqli=connexion();
 
